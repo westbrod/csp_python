eb7e1188f51772d64502b9b4a8d90a79816215bb
DeAndre Westbrook <westbrod@Student.psdr3.org>
2020-01-13 12:43:24 -0600

    added username and password attrabutes

--- unit_2/a214class.py
+++ unit_2/a214class.py
@@ -3,4 +3,28 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("400x300")
 
+user_label = tk.Label(root, text='Username')
+user_label.pack()
+
+user_text =tk.Entry(root)
+user_text.pack()
+user_submit = user_text.get()
+
+
+user_label = tk.Label(root, text='Password ')
+user_label.pack()
+
+Pass_text =tk.Entry(root)
+Pass_text.pack()
+Pass_submit = Pass_text.get()
+
+def callback():
+    print ("click!")
+
+b = button(master, text="Submit", command=callback)
+b.pack()
+
+
+
+
 root.mainloop()
