452db890fafac06a5ccdbe63af5b2450082ed495
DeAndre Westbrook <westbrod@Student.psdr3.org>
2020-01-19 19:23:03 -0600

    orginized code

--- unit_2/a214class.py
+++ unit_2/a214class.py
@@ -5,29 +5,8 @@ def callback():
     print ("User:", user_entry.get())
     print ("Pass:", Pass_text.get())
 
-
-root = tk.Tk()
-root.geometry("400x300")
-
-user_label = tk.Label(root, text='Username')
-user_label.pack()
-user_entry = tk.Entry(root)
-user_entry.pack(pady=5)
-
-user_label = tk.Label(root, text='Password ')
-user_label.pack()
-
-Pass_text = tk.Entry(root, show='')
-Pass_text.pack()
-
-
-Pass_submit = tk.Button(root, text= 'submit', command= callback)
-Pass_submit.pack()
-
-
-
 def pass_check():
-    if len(Pass_text.get()) <= 8:
+    if len(Pass_text.get()) >= 8:
         print("Good")
     else:
         print("Please enter a password longer than 8 characters. ")
@@ -47,13 +26,26 @@ def submit_sign_up():
 
 def is_valid_password(password):
     if len(password) < 8:
+        pass
 
 
+root = tk.Tk()
+root.geometry("400x300")
 
-    
+user_label = tk.Label(root, text='Username')
+user_label.pack()
+user_entry = tk.Entry(root)
+user_entry.pack(pady=5)
+
+user_label = tk.Label(root, text='Password ')
+user_label.pack()
+
+Pass_text = tk.Entry(root, show='')
+Pass_text.pack()
 
 
-
+Login= tk.Button(root, text= 'Login', command= callback)
+Login.pack()
 
 
 signup_submit = tk.Button(root, text= 'sighup', command= pass_check )
