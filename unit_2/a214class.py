8b948f9fd2f284e09c3164420068682d411b5905
DeAndre Westbrook <westbrod@Student.psdr3.org>
2020-01-23 13:05:42 -0600

    finished

--- unit_2/a214class.py
+++ unit_2/a214class.py
@@ -1,42 +1,41 @@
 import tkinter as tk 
 import string
+from passlib.hash import pbkdf2_sha256
+
+#pbkdf2_sha256.verify(Pass_text.get(), users[user_entry.get()])
 
 result = ("you have been signed up")
 users = {}
 
+
 def callback():
-    print ("User:", user_entry.get())
-    print ("Pass:", Pass_text.get())
-    
+    if user_entry.get() not in users:
+        result_label.config(text = 'Username not found. ')
+    elif pbkdf2_sha256.verify(Pass_text.get(), users[user_entry.get()]):
+        result_label.config(text="valid log in")
+    else:
+        result_label.config(text='invalid password')
 
 
-# def import_users():
-#     # users ['simmonsjo5'] = 'password'
-#     users [user_entry.get] = pass_text.get 
+
 def submit_sign_up():
     namelater = Pass_text.get()
-    
+    hash = pbkdf2_sha256.hash(Pass_text.get())   
 
     if user_entry.get() in users:
-        print('username is already in use.')
+        result_label.config(text='username is already in use.')
+
     else: 
-        user1 = users[user_entry.get()] = Pass_text.get()
+        user1 = users[user_entry.get()] = hash
         if is_valid_password(namelater):
             result_label.config(text= 'valid sign up ')
         else:
-            result_label.config(text= 'Not a valid sign up') 
-        for username in users:
-            pass
-        
-    print(users)
+            result_label.config(text= 'Not a valid sign up')
 
-    
- 
 
 def is_valid_password(password):
     if len(password) < 8:
-        return False  
-    # return True
+        return False 
 
     has_upper = False 
     has_lower = False 
@@ -56,46 +55,35 @@ def is_valid_password(password):
     return has_lower and has_upper and has_digit and has_special
 
 
-
-        
-
-
 root = tk.Tk()
 root.geometry("400x300")
 
-#user label anf txt box 
+#user label and txt box 
 user_label = tk.Label(root, text='Username')
 user_label.pack()
 
 user_entry = tk.Entry(root)
 user_entry.pack(pady=5)
 
-user_label = tk.Label(root, text='Password ')
-user_label.pack()
+pass_label = tk.Label(root, text='Password ')
+pass_label.pack()
+
+  
 
 #password txt box and label
 Pass_text = tk.Entry(root, show='')
 Pass_text.pack()
 
+login = tk.Button(root, text= 'Login', command= callback)
+login.pack()
 
-Login= tk.Button(root, text= 'Login', command= callback)
-Login.pack()
-
-
-user_and_pass = user_entry
 
 result_label = tk.Label(root, text = 'result')
 result_label.pack(pady=20)
 
 
-
-
-
 signup_submit = tk.Button(root, text= 'sighup', command= submit_sign_up )
 signup_submit.pack()
-
-
-
 
 
 root.mainloop()
